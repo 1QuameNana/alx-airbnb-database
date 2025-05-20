22964021f0f2ce000244dbcee4bb0fedf7dc0d09
Kyei Cundy <nabcoasfeg14756@gmail.com>
2025-05-20 15:08:05 +0100

    left join

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -53,3 +53,17 @@ FROM
     bookings
 LEFT JOIN 
     users ON users.id = bookings.user_id;
+
+SELECT 
+    properties.id AS property_id,
+    properties.title,
+    properties.location,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id
+ORDER BY 
+    properties.title ASC;
